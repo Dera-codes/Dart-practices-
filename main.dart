260b6db2cb5043e1e1e8d7d3146cbf9c1e5448bf
Nnamdi Chidera Emmanuel <69612512+Dera-codes@users.noreply.github.com>
2023-05-03 17:01:57 +0100

    Add files via upload
    Traffic light signs using if else statements in dart.

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  String trafficlight = 'green';
+  if (trafficlight == 'red') {
+    print('Stop');
+  } else if (trafficlight == 'yellow') {
+    print('Ready');
+  } else if (trafficlight == 'green') {
+    print('Go');
+  }
+}
